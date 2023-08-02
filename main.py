c6fc9eddb6f03151ba7a3265289ef1fd59123dd0
Githero10 <112308352+Githero10@users.noreply.github.com>
2023-08-02 13:35:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_
     app.send_message(message.chat.id, f"__👋 Hi **{message.from_user.mention}**, i am Link Bypasser Bot, just send me any supported links and i will you get you results.\nCheckout /help to Read More__",
     reply_markup=InlineKeyboardMarkup([
         [ InlineKeyboardButton("🌐 Source Code", url="https://github.com/bipinkrish/Link-Bypasser-Bot")],
-        [ InlineKeyboardButton("Replit", url="https://replit.com/@bipinkrish/Link-Bypasser#app.py") ]]), 
+        [ InlineKeyboardButton("🔔 Subscribe", url="https://youtube.com/@all_rounder_for_all") ]]), 
         reply_to_message_id=message.id)
 
 
@@ -146,5 +146,6 @@ def docfile(client: pyrogram.client.Client, message: pyrogram.types.messages_and
 
 
 # server loop
-print("Bot Starting")
+print("Bot Started Enjoy 😊")
 app.run()
+    
